1d9c1e0d9b6ecdc8ad2e9300071ed798bc08c1ec
Giancarlo Pereira <giancarlo.pereira@columbia.edu>
2024-12-13 11:05:12 -0500

    testing orientation of wall texture graphics final project

--- graphics/final_project/lib.js
+++ graphics/final_project/lib.js
@@ -318,8 +318,8 @@ function Cell(s, r, c, g, d) {
       // draw each wall
       if (walls['top'])    M.S().move(add(pos, [ 0, size/2, size/2])).scale(size/2, size/2, size/100).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
       if (walls['bottom']) M.S().move(add(pos, [0, size/2, -size/2])).scale(size/2, size/2, size/100).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
-      if (walls['right'])  M.S().move(add(pos, [-size/2, size/2, 0])).turnZ(pi/2).scale(size/100, size/2, size/2).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
-      if (walls['left'])   M.S().move(add(pos, [ size/2, size/2, 0])).turnZ(pi/2).scale(size/100, size/2, size/2).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
+      if (walls['right'])  M.S().move(add(pos, [-size/2, size/2, 0])).turnX(pi/2).scale(size/100, size/2, size/2).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
+      if (walls['left'])   M.S().move(add(pos, [ size/2, size/2, 0])).turnX(pi/2).scale(size/100, size/2, size/2).draw(myCube, wallColor, 1, wallTex, wallBtex).R();
 
       // draw floor
       if (mode!=='debug') {
